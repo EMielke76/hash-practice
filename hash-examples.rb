1959096baec1e5da333883398641cc67eb7c8ce9
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-08 17:38:53 -0600

    Add customer hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -8,5 +8,15 @@ luna = {
   "favorite_activity" => 'Bed Time',
   "has_shots" => true,
   "likes_strangers" => false,
-  "likes_the_vet" => false,
+  "likes_the_vet" => false
+}
+
+customer = {
+  "name" => 'Roberto',
+  "age" => 54,
+  "rewards_member" => true,
+  "linked_account" => true,
+  "paperless_account" => true,
+  "address" => '123 Fake Street, Anytown, CO 80120',
+  "most_purchased_items" => ['tequila', 'limes', 'agave', 'salt']
 }
