a90f8a8ebe43ee16001a40484633469235b72b1b
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-08 17:29:18 -0600

    Add Luna hash

---
+++ hash-examples.rb
@@ -0,0 +1,12 @@
+luna = {
+  "name" => 'Luna',
+  "age" => 2,
+  "breed" => 'Blue Heeler',
+  "favorite_toy" => 'A damned stick',
+  "best_friend" => 'Cici',
+  "favorite_human" => 'Eric',
+  "favorite_activity" => 'Bed Time',
+  "has_shots" => true,
+  "likes_strangers" => false,
+  "likes_the_vet" => false,
+}
